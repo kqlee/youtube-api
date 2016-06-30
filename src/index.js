9951677547b0c1c9d0a135ac2a904477c8ce1d2c
Kent <kentqlee@gmail.com>
2016-06-29 21:25:32 -0700

    Built out search functionality for youtube

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
+const request = require('request');
+const YouTube = require('youtube-node');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const Youtube = {
+const youTube = new YouTube();
+youTube.setKey(process.env.YOUTUBE_API_KEY);
 
+const YouTubeSearch = {
 
+  Search: (query, numVids, callback) => {
+
+    youTube.search(query, numVids, (error, result) => {
+      if (error) {
+        callback(error);
+      }
+      callback(null, result);
+    });
+  }
 
 };
 
-module.exports = Youtube;
+module.exports = YouTubeSearch;
